27d6fd269440897603e3d2b6d283c641e748cf37
Amil A <amil7.abdullayev@gmail.com>
2024-09-16 20:36:52 +0400

    feat(ui): step 1 phase 2

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+      <div className="card test logo">
+          <h2 style={{ color: 'red', backgroundColor: 'blue' }} >Card Title</h2>
+          <p>Card Description</p>
+          <div>
+              <h2>wadfawdf</h2>
+          </div>
+      </div>
+  )
+}
+
+export default Card;
